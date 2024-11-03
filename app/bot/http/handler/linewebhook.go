94fc950fb4c20c7078b9d09eaf6f52ef6a1ff320
Li-Ting Tsai <lt@ltt.pw>
2024-11-04 00:13:10 +0800

    try audio chat complete

--- app/bot/http/handler/linewebhook.go
+++ app/bot/http/handler/linewebhook.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 	"github.com/line/line-bot-sdk-go/v8/linebot/webhook"
@@ -23,6 +25,18 @@ const (
 	If you are not sure about something in the translation result, you must add comments at the end.
 
 	If it is hard to translate, you can just describe it.
+	`
+
+	SPEAKER_PROMPT = `
+	You are a language teacher. Let's work out the translation step by step.
+	For the input, you will translate it into English(US), Japanese, 繁體中文(Taiwan).
+	And you will output each language's result.
+
+	If you are not sure about something in the translation result, you must add comments at the end.
+
+	If it is hard to translate, you can just describe it.
+
+	After the translation, you will expliain more the context in 繁體中文(Taiwan).
 	`
 )
 
@@ -180,12 +194,69 @@ func (im LineWebhookHandler) handleImageMessage(ctx context.Context, e webhook.M
 	}
 }
 
+func (im LineWebhookHandler) handleAudioMessage(ctx context.Context, e webhook.MessageEvent, message webhook.AudioMessageContent) {
+	im.replyFlyingMoneyMessage(e.ReplyToken)
+	slog.Debug("audio message", slog.Any("message", message))
+
+	resp, err := im.blobAPI.GetMessageContent(message.Id)
+	if err != nil {
+		slog.Error("handleAudioMessage", slog.Any("err", err))
+
+		return
+	}
+	defer resp.Body.Close()
+
+	// sting buf as Writer
+	var stringBuilder strings.Builder
+	b64Encoder := base64.NewEncoder(base64.StdEncoding, &stringBuilder)
+
+	teeReader := io.TeeReader(resp.Body, b64Encoder)
+	_, err = io.ReadAll(teeReader)
+	if err != nil {
+		slog.Error("io.Readall teeReader", slog.Any("err", err))
+	}
+	b64Encoder.Close()
+
+	ctx = context.WithoutCancel(ctx)
+
+	reply, err := im.openaiClient.Chat.Completions.New(ctx,
+		openai.ChatCompletionNewParams{
+			Model:      openai.F(openai.ChatModelGPT4oAudioPreview),
+			Modalities: openai.F([]openai.ChatCompletionModality{openai.ChatCompletionModalityText, openai.ChatCompletionModalityAudio}),
+			Audio: openai.F(openai.ChatCompletionAudioParam{
+				Format: openai.F(openai.ChatCompletionAudioParamFormatMP3),
+				Voice:  openai.F(openai.ChatCompletionAudioParamVoiceSage),
+			}),
+			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+				openai.SystemMessage(SPEAKER_PROMPT),
+				openai.UserMessage("how to convert aac to mp3 freely"),
+			}),
+		})
+	if err != nil {
+		slog.Error("genreate audio error", slog.Any("err", err))
+	}
+
+	slog.Info(reply.Choices[0].Message.Content)
+	localPath := "static/" + message.Id + ".mp3"
+
+	newFile, err := os.Create(localPath)
+	if err != nil {
+		return
+	}
+	defer newFile.Close()
+
+	b64Decoder := base64.NewDecoder(base64.StdEncoding, strings.NewReader(reply.Choices[0].Message.Audio.Data))
+	io.Copy(newFile, b64Decoder)
+}
+
 func (im LineWebhookHandler) handleMessageEvent(ctx context.Context, e webhook.MessageEvent) {
 	switch message := e.Message.(type) {
 	case webhook.TextMessageContent:
 		im.handleTextMessage(ctx, e, message)
 	case webhook.ImageMessageContent:
 		im.handleImageMessage(ctx, e, message)
+	case webhook.AudioMessageContent:
+		im.handleAudioMessage(ctx, e, message)
 	default:
 		slog.Warn("Unsupported message content", slog.String("event_type", e.GetType()))
 	}
